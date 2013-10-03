1ba650163f6a4ad11886988038b8916a75498dea
GUIDOBOT <guidobot.gbot@gmail.com>
2013-10-03 14:48:21 -0300

    fixed tabs

--- gb138/gbZip.cpp
+++ gb138/gbZip.cpp
@@ -2,13 +2,12 @@
 
 int gbZip :: entryIsEboot(const char * name, int mode)
 {
-	char eboot_names[][13] = {"wmenu.bin\0", "EBOOT.PBP\0", "FBOOT.PBP\0", "VBOOT.PBP\0"};
-    char * aux = NULL;
+	char eboot_names[][13] = {"wmenu.bin\0", "EBOOT.PBP\0", "FBOOT.PBP\0", "VBOOT.PBP\0"}, * aux = NULL;
 	int i;
 	if(mode) aux = strrchr(name, '/');
 	else aux = strchr(name, '/');
 	if(!aux) return 0;
-    for(i=0;i<4;i++) if(!strcmp(aux+1, eboot_names[i])) return 1;
+	for(i=0;i<4;i++) if(!strcmp(aux+1, eboot_names[i])) return 1;
 	return 0;
 };
 
@@ -26,32 +25,30 @@ void gbZip :: fixname(char * name)
 int gbZip :: unzip(const char * file, const char * root, const char * eboot_name)
 {
 	structLocalFileHeader fileHeader;
-    FILE * a, * b;
-    int centralDirFound = 0, copy_count = 0;
+	FILE * a, * b;
+	int centralDirFound = 0, copy_count = 0;
 	char * save_path = NULL, * entry_name = NULL, copy_buffer[COPYING_BUFFER];
 	unsigned magic_check, i;
-    memset(&fileHeader, 0, sizeof(structLocalFileHeader));
-	//memset(save_path, 0, ZIP_ENTRY_NAME+100);
-	
-    a = fopen(file, "rb");
-    fseek(a, 0, SEEK_SET);
+	memset(&fileHeader, 0, sizeof(structLocalFileHeader));
 
-    while(!centralDirFound)
-    {
-        fread(&magic_check, sizeof(unsigned), 1, a);
-        if(magic_check == CENTRAL_DIR_MAGIC) centralDirFound = 1;
-        else
-        {
-            //memset(name, 0, ZIP_ENTRY_NAME);
-            fseek(a, 4, 1);
-            fread(&fileHeader.compression, sizeof(unsigned short), 1, a);
-            fseek(a, 8, 1);
-            fread(&fileHeader.compressedSize, sizeof(unsigned), 1, a);
-            fseek(a, 4, 1);
-            fread(&fileHeader.filenameLen, sizeof(unsigned), 1, a);
+	a = fopen(file, "rb");
+	fseek(a, 0, SEEK_SET);
+
+	while(!centralDirFound)
+	{
+		fread(&magic_check, sizeof(unsigned), 1, a);
+		if(magic_check == CENTRAL_DIR_MAGIC) centralDirFound = 1;
+		else
+		{
+			fseek(a, 4, 1);
+			fread(&fileHeader.compression, sizeof(unsigned short), 1, a);
+			fseek(a, 8, 1);
+			fread(&fileHeader.compressedSize, sizeof(unsigned), 1, a);
+			fseek(a, 4, 1);
+			fread(&fileHeader.filenameLen, sizeof(unsigned), 1, a);
 			
 			entry_name = (char *)malloc(fileHeader.filenameLen+1);
-            fread(entry_name, fileHeader.filenameLen, 1, a);
+			fread(entry_name, fileHeader.filenameLen, 1, a);
 			*(entry_name + fileHeader.filenameLen) = '\0';
 			if(gbZip::entryIsEboot(entry_name, 1)) strcpy(strrchr(entry_name, '/')+1, eboot_name);
 			gbZip::fixname(entry_name);
@@ -73,14 +70,6 @@ int gbZip :: unzip(const char * file, const char * root, const char * eboot_name
 					fclose(a);
 					return gbZip::ZIP_ERROR;
 				};
-				/*if(gbExplorer::DirExist(save_path, DONT_CREATE)!=EXPLORER_OK)
-				{
-					if(gbExplorer::CreateDir(save_path)!=EXPLORER_OK) 
-					{
-						fclose(a);
-						return ZIP_ERROR;
-					};
-				};*/
 			}
 			else //Is it a file?
 			{
@@ -109,58 +98,57 @@ int gbZip :: unzip(const char * file, const char * root, const char * eboot_name
 			};
 			free(entry_name);
 			free(save_path);
-        };
-    };
-    fclose(a);
+		};
+	};
+	fclose(a);
 	pspDebugScreenClear();
 	__printf("Installed. Refreshing list..");
-    return gbZip::ZIP_OK;
+	return gbZip::ZIP_OK;
 };
 
 unsigned gbZip :: getEbootOffset(const char * file)
 {
-    structLocalFileHeader fileHeader;
-    unsigned magic_check, offset = 0;
-    int centralDirFound = 0;
+	structLocalFileHeader fileHeader;
+	unsigned magic_check, offset = 0;
+	int centralDirFound = 0;
 	char * entry_name = NULL;
 	FILE * a = fopen(file, "rb");
 
-    fread(&magic_check, sizeof(unsigned), 1, a);
-    if(magic_check != FILE_MAGIC)
-    {
-        fclose(a);
-        return 0;
-    };
-    fseek(a, 0, SEEK_SET);
+	fread(&magic_check, sizeof(unsigned), 1, a);
+	if(magic_check != FILE_MAGIC)
+	{
+		fclose(a);
+		return 0;
+	};
+	fseek(a, 0, SEEK_SET);
 	memset(&fileHeader, 0, sizeof(structLocalFileHeader));
-    while(!centralDirFound && !offset)
-    {
-        fread(&magic_check, sizeof(unsigned), 1, a);
-        if(magic_check == CENTRAL_DIR_MAGIC) centralDirFound = 1;
-        else
-        {
-            fseek(a, 4, SEEK_CUR);
-            fread(&fileHeader.compression, sizeof(unsigned short), 1, a);
-            if(fileHeader.compression) //Zip is compressed, discard file
-            {
-                fclose(a);
-                return 0;
-            };
-            fseek(a, 8, SEEK_CUR);
-            fread(&fileHeader.compressedSize, sizeof(unsigned), 1, a);
-            fseek(a, 4, SEEK_CUR);
-            fread(&fileHeader.filenameLen, sizeof(unsigned), 1, a);
+	while(!centralDirFound && !offset)
+	{
+		fread(&magic_check, sizeof(unsigned), 1, a);
+		if(magic_check == CENTRAL_DIR_MAGIC) centralDirFound = 1;
+		else
+		{
+			fseek(a, 4, SEEK_CUR);
+			fread(&fileHeader.compression, sizeof(unsigned short), 1, a);
+			if(fileHeader.compression) //Zip is compressed, discard file
+			{
+				fclose(a);
+				return 0;
+			};
+			fseek(a, 8, SEEK_CUR);
+			fread(&fileHeader.compressedSize, sizeof(unsigned), 1, a);
+			fseek(a, 4, SEEK_CUR);
+			fread(&fileHeader.filenameLen, sizeof(unsigned), 1, a);
 			entry_name = (char *)malloc(fileHeader.filenameLen+1);
-            fread(entry_name, fileHeader.filenameLen, 1, a);
+			fread(entry_name, fileHeader.filenameLen, 1, a);
 			*(entry_name+fileHeader.filenameLen) = '\0';
-			//__printf("ZIP: %s-\n", entry_name);
 			fseek(a, fileHeader.extrafieldLen, 1);
-            if(gbZip::entryIsEboot(entry_name, 0)) offset = ftell(a);
-            fseek(a, fileHeader.compressedSize, 1);
+			if(gbZip::entryIsEboot(entry_name, 0)) offset = ftell(a);
+			fseek(a, fileHeader.compressedSize, 1);
 			free(entry_name);
-        };
-    };
+		};
+	};
 
-    fclose(a);
-    return offset;
+	fclose(a);
+	return offset;
 };
